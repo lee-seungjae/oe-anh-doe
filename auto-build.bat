b319a79b28dbb81fab2be1b32c2fbcc20f8993af
Seungjae Lee <distynova@gmail.com>
2019-04-15 16:44:55 +0900

    빌드 스크립트 손봄

--- auto-build.bat
+++ auto-build.bat
@@ -3,5 +3,5 @@
 REM tsc 빌드 안 되어 있을 때 webpack 실행하면 -w 모드에 들어가지 않고 바로 빠져나옴
 call tsc
 
-start cmd /k "title typescript && tsc -w"
-start cmd /k "title webpack && npx webpack --mode=development -w"
+start /b tsc -w
+npx webpack --mode=development -w
